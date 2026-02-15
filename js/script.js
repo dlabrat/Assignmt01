45f65447113a4067b425045f90d94c675844e542
dlabrat <107654952+dlabrat@users.noreply.github.com>
2026-02-15 12:59:58 -0800

    step one on .js file saved

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// STEP 1
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// STEP 2
+
+// STEP 3
+
+
+// STEP 4
+
+// STEP 5
+
+// STEP 6
+
+// STEP 7
+
+// STEP 8
+

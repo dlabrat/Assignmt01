9203e807c02891d1861baf2df689fcd7c48d4d91
dlabrat <107654952+dlabrat@users.noreply.github.com>
2026-02-15 13:21:17 -0800

    step 2 done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-// STEP 1
-let someMonth
-function theMonth()
-let currentMonth
-let summerMonth
-let myLibraryFunction
+// STEP 1 (comment with cntr and /,^/)
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
 
 // STEP 2
+//numeric literal expression: 
+53
+
+//a string literal expression: 
+'Hi there'
+
+//a Boolean literal expression:
+true 
+// a null literal expression:
+null
 
 // STEP 3
-
 
 // STEP 4
 
